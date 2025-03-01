10ca38b1d6f92376c2bcaaf18f1ff3783b5b6c87
Kristy <kristy.lee1218@outlook.com>
2025-02-28 22:26:33 -0600

    Added carousel

--- site.js
+++ site.js
@@ -33,3 +33,51 @@ const secretmessage = "The only thing we have to fear is fear itself. -Franklin 
 
 //Run the message to the local storage
 localStorage.setItem(key, secretmessage)
+
+//ASSIGNMENT 5 - CAROUSEL
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+showImages()
+
+//addEventListener - PREVIOUS BUTTON
+const prevbutton = document.querySelector('#prev')
+prevbutton.addEventListener('click', () => {
+    currentImage--
+    showImages() //NEED TO CALL showImages
+});
+
+//addEventListner - NEXT BUTTON
+const nextbutton = document.querySelector('#next')
+nextbutton.addEventListener('click', () => {
+    currentImage++
+    showImages() //NEED TO CALL showImages
+});
+
+//SET THE TIME to move to the next image
+function nextimage() {
+    currentImage++ //MOVE THE NEXT IMAGE
+    showImages() //SHOW THE IMAGES
+}
+
+setInterval(() => {
+    //code to run Every 5 seconds
+    nextimage() //Call the function
+},5000)
+
+showImages()
